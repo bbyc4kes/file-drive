722c7088d262e3cdc074ef6f605754b6881e45e5
Max <azimov.workspace@gmail.com>
2024-03-29 20:52:22 +0100

    implemented authorization based of tokenIdentifier and user id

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,35 @@
 
 import { Button } from '@/components/ui/button'
 import { api } from '@/convex/_generated/api'
-import { SignInButton, SignOutButton, SignedIn, SignedOut } from '@clerk/nextjs'
+import { useOrganization, useUser } from '@clerk/nextjs'
 import { useMutation, useQuery } from 'convex/react'
 
 export default function Home() {
+  const organization = useOrganization()
+  const user = useUser()
+
+  let orgId: string | undefined = undefined
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id
+  }
+
   const createFile = useMutation(api.files.createFile)
-  const files = useQuery(api.files.getFiles)
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignedIn>
-        <SignOutButton>
-          <Button>Sign Out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
       {files?.map((file) => (
         <div key={file._id}>{file.name}</div>
       ))}
 
       <Button
-        onClick={() =>
+        onClick={() => {
+          if (!orgId) return
           createFile({
             name: 'hey123',
+            orgId,
           })
-        }
+        }}
       >
         Click Me
       </Button>
