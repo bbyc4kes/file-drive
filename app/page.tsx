5594da50366f3bfb11d2ba1bd1f6fa9d0bc85b61
Max <azimov.workspace@gmail.com>
2024-04-01 00:10:44 +0200

    added loading spinner

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import { useQuery } from 'convex/react'
 
 import UploadButton from './upload-button'
 import { FileCard } from './file-card'
-import Image from 'next/image'
-import { Loader2 } from 'lucide-react'
 
 export default function Home() {
   const organization = useOrganization()
@@ -19,47 +17,18 @@ export default function Home() {
   }
 
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip')
-  const isLoading = files === undefined || files === null
 
   return (
     <main className="container mx-auto pt-12">
-      {isLoading && (
-        <div className="flex flex-col gap-3 w-full items-center mt-16">
-          <Loader2 className="h-12 w-12 animate-spin opacity-90 text-gray-500" />
-          <div className="text-sm md:text-md">Loading images...</div>
-        </div>
-      )}
-
-      {!isLoading && files?.length === 0 && (
-        <div className="flex flex-col gap-6 w-full items-center mt-16">
-          <Image
-            src="/empty.svg"
-            width={300}
-            height={300}
-            className="opacity-95 h-[200px] w-[200px] md:max-h-[300px] md:w-[300px]"
-            alt="placeholder for workspace"
-          />
-          <div className="text-xl md:text-2xl">
-            You have no files in your workspace now.
-          </div>
-          <UploadButton />
-        </div>
-      )}
-
-      {!isLoading && files.length > 0 && (
-        <>
-          <div className="flex justify-between items-center mb-8">
-            <h1 className="text-4xl font-bold">Your Files</h1>
-            <UploadButton />
-          </div>
-
-          <div className="flex flex-wrap gap-4">
-            {files?.map((file) => (
-              <FileCard key={file._id} file={file} />
-            ))}
-          </div>
-        </>
-      )}
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-bold">Your Files</h1>
+        <UploadButton />
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        {files?.map((file) => (
+          <FileCard key={file._id} file={file} />
+        ))}
+      </div>
     </main>
   )
 }
