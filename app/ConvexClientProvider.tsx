4d5dbe95ff114f42e9dcf4d1f0e837e9444ba752
Max <azimov.workspace@gmail.com>
2024-04-07 17:04:46 +0200

    configuring

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from 'convex/react'
 import { ClerkProvider, useAuth } from '@clerk/nextjs'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
 
-const convex = new ConvexReactClient(process.env.CONVEX_URL!)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function ConvexClientProvider({
   children,
